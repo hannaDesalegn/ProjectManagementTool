a95468eb82fd55bf3e16dc156bbaa48791f31a33
hannaDesalegn <hannadesalegn21@gmail.com>
2026-02-05 19:52:58 +0300

    change folder of jwt.js from config folder to utils folder

--- src/config/db.js
+++
@@ -1,7 +0,0 @@
-const hashedPassword = await bcrypt.hash("123456", 10);
-await prisma.user.create({
-    data: {
-        email: "yo@taskflow.com",
-        password_hash: hashedPassword
-    }
-});
