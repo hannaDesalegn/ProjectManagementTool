332fcc2e8f9c53c2d117c598e0e0c66c7bc8b113
hannaDesalegn <hannadesalegn21@gmail.com>
2026-02-06 13:50:23 +0300

    add error handling middleware

---
+++ src/middleware/error.middleware.js
@@ -0,0 +1,8 @@
+// middleware/error.middleware.js
+export default (err, req, res, next) => {
+  const status = err.status || 500;
+  res.status(status).json({
+    success: false,
+    error: err.message || "Internal Server Error",
+  });
+};
