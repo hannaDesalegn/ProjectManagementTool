bdacce810506838213833f49d5deee8e6b7556e4
hannaDesalegn <hannadesalegn21@gmail.com>
2026-02-06 14:07:01 +0300

    Update import/export style

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const app = require("./app");
+// server.js
+import app from "./app.js";
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
