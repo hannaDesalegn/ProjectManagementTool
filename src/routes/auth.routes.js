3dd1facd78416a519ed3ad4ac21fc22364d0377f
yordi-7-cyber <tyordi09@gmail.com>
2026-02-06 23:24:15 -0900

    Haytask

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import authController from "../controllers/auth.controller.js";
-
-router.post("/register", authController.register);
-router.post("/login", authController.login);
-
-export default router;
